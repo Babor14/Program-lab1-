8d40ebb8834e63c6903c94222d5bd216faeab65c
rekun <rekun@LAPTOP-JS11QQ90>
2023-10-01 22:38:54 +0200

    Добавьте файлы проекта.

---
+++ Program/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] sentences = { "Это первое предложение", "Это второе предложение", "Это третье предложение" };
+        char searchChar = 'о';
+
+        int totalCount = 0; // Общее количество заданной буквы в предложениях
+
+        Thread[] threads = new Thread[sentences.Length]; // Массив потоков
+
+        for (int i = 0; i < sentences.Length; i++)
+        {
+            int index = i;
+
+            threads[i] = new Thread(() =>
+            {
+                int countInSentence = GetLetterCount(sentences[index], searchChar);
+                Interlocked.Add(ref totalCount, countInSentence); 
+            });
+
+            threads[i].Start();
+        }
+
+        for (int i = 0; i < sentences.Length; i++)
+        {
+            threads[i].Join(); 
+        }
+
+        Console.WriteLine("Общее количество букв '{0}': {1}", searchChar, totalCount);
+    }
+
+    static int GetLetterCount(string sentence, char searchChar)
+    {
+        int count = 0;
+
+        foreach (char c in sentence)
+        {
+            if (c == searchChar)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
